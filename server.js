636abd371298766f52792e35e566978f71ec351c
madhav dhavala <madhavdhavala0@gmail.com>
2025-11-24 11:14:57 +0530

    replaced css with scss

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ connectDB();
 app.use(helmet({ contentSecurityPolicy: false }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.static(path.join(__dirname, 'views'))); // ⬅ ADD THIS
 
 // Set EJS as view engine
 app.set('view engine', 'ejs');
